06682d1795de24d7b07889f1e9b3fc5482757f83
levchuk <swankyyy1@gmail.com>
2024-12-28 12:19:34 +0300

    some structuree improvements

--- app/parser/parser.py
+++ app/parser/parser.py
@@ -1,21 +1,9 @@
 import requests
 import asyncio
 from models import Item
+from settings import settings
 
-headers = {
-    "Accept": "*/*",
-    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Connection": "keep-alive",
-    "Origin": "https://www.wildberries.by",
-    "Referer": "https://www.wildberries.by/",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "cross-site",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-    "sec-ch-ua": 'Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "macOS",
-}
+headers = settings.PARSER_HEADERS
 
 
 class ItemParse:
--- app/src/settings.py
+++
@@ -1,7 +0,0 @@
-class Settings:
-    DB_URL = 'postgresql+asyncpg://db:db@localhost:5432/db'
-    DB_ECHO = False
-    
-    
-    
-settings = Settings()
